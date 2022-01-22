03246f35dd7d2af157db24c48a709f8594c7bb91
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-22 22:19:55 +0530

    Add a firebase.js for configuring firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCisQOoWbflNo-oyROkicq_g7RZHjBV4CQ",
+  authDomain: "react-hooks-blog-e02af.firebaseapp.com",
+  projectId: "react-hooks-blog-e02af",
+  storageBucket: "react-hooks-blog-e02af.appspot.com",
+  messagingSenderId: "390491815690",
+  appId: "1:390491815690:web:5f830e77cb81fb6cc184f3"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);

ace46868ca520f3e7b7ad4b0b9fe5709c5091f22
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-08 13:45:00 +0530

    Create Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+import {Link} from 'react-router-dom';
+import { firestore } from "../firebase";
+
+
 function Home() {
-    return (
-      <div className="">Home</div>
-    );
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+    .collection('posts')
+    .get()
+    .then((snapshot) => {
+      const posts = snapshot.docs.map((doc) => {
+        return {
+          id : doc.id,
+          ...doc.data()
+        };
+      });
+
+      console.log('posts', posts);
+      setPosts(posts);
+    })
+  },[]);
+
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id ="blog-by">Sarthak</div>
+
+      {posts.map((post,index) => (
+        <div className="post" key={`post-${index}`}>
+        <Link to={`/post/${post.id}`}>
+          <h3> {post.title} </h3>
+        </Link>
+
+        <p> {post.subTitle} </p>
+        </div>
+      ))}
+    </div>
+  );
 }
   
 export default Home;

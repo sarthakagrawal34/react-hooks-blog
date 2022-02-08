73658e56862d16bbf81787aff1080bca220f1bc8
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-08 13:03:20 +0530

    Make changes in create post using custom hooks

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import {firestore} from '../firebase'
+// import { useState } from "react";
+import {firestore} from '../firebase';
+import {useFormInput} from './hooks'
 
 function CreatePost() {
-  const [title,setTitle] = useState();
-  const [subTitle,setSubTitle] = useState();
-  const [content,setContent] = useState();
+  const title= useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
 
   function handleSubmit (e){
     e.preventDefault();
@@ -14,9 +15,9 @@ function CreatePost() {
     console.log('content', content);
 
     firestore.collection('posts').add({
-      title,
-      subTitle,
-      content,
+      title : title.value,
+      subTitle : subTitle.value,
+      content : content.value,
       createdAt : new Date(),
     })
 
@@ -28,15 +29,19 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e)=>setTitle(e.target.value)}/>
+          {/* <input value={title} onChange={(e)=>setTitle(e.target.value)}/> */}
+          <input {...title}/>
+
         </div>
         <div className="form-field">
           <label>Sub Title</label>
-          <input value={subTitle} onChange={(e)=>setSubTitle(e.target.value)}/>
+          {/* <input value={subTitle} onChange={(e)=>setSubTitle(e.target.value)}/> */}
+          <input {...subTitle}/>
         </div>
         <div className="form-field">
           <label>Content</label>
-          <textarea value={content} onChange={(e)=>setContent(e.target.value)}></textarea>
+          {/* <textarea value={content} onChange={(e)=>setContent(e.target.value)}></textarea> */}
+          <textarea {...content}></textarea>
         </div>
 
         <button className="create-post-btn">
---
+++ src/components/hooks.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function useFormInput (initialValue){
+    const [value, setValue] = useState(initialValue);
+
+    function handleChange(e){
+        setValue(e.target.value);
+    }
+
+    return{
+        value,
+        onChange : handleChange,
+    };
+
+}

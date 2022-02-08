a4a6283603f40e45e5504ebeaaa3d1374826534f
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-08 12:33:21 +0530

    Implement Create Post Component using hooks

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 function CreatePost() {
-    return (
-      <div className="">CreatePost</div>
-    );
+  const [title,setTitle] = useState();
+  const [subTitle,setSubTitle] = useState();
+  const [content,setContent] = useState();
+
+  function handleSubmit (e){
+    e.preventDefault();
+
+    console.log('title', title);
+    console.log('sub title', subTitle);
+    console.log('content', content);
+
+  }
+
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          <input value={title} onChange={(e)=>setTitle(e.target.value)}/>
+        </div>
+        <div className="form-field">
+          <label>Sub Title</label>
+          <input value={subTitle} onChange={(e)=>setSubTitle(e.target.value)}/>
+        </div>
+        <div className="form-field">
+          <label>Content</label>
+          <textarea value={content} onChange={(e)=>setContent(e.target.value)}></textarea>
+        </div>
+
+        <button className="create-post-btn">
+          Create Post
+        </button>
+      </form>
+    </div>
+  );
 }
   
 export default CreatePost;

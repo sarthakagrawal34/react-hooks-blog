3511694dacbc9812195ff2f4085975b8e3128a31
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-02-09 14:00:40 +0530

    Use styled-components for doing styling of post component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,47 @@
 import { useState, useEffect } from "react";
 import {Link} from 'react-router-dom';
 import { firestore } from "../firebase";
+import styled from "styled-components";
+
+// const BlogHeading = styled.h1`
+//   text-align: center;
+//   color: #2196f3;
+//   margin-bottom: 2px;
+// `;
+
+// Using styled components to style our subtitle paragraph
+const PostSubTitle = styled.p`
+  font-size: 13px;
+  color : grey;
+`;
+
+// Now using this styled-components for many other things also like nested class, media queries etc
+const Post = styled.div`
+  border: 1px solid #e1e1e1;
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+
+  // &:hover{
+  //   border: 1px solid red;
+  // }
+
+  h3{
+    margin: 0;
+    padding: 0;
+    font-size: 25px;
+    font-weight: bold;
+    color: black;
+  }
+
+  a{
+    text-decoration: none;
+  }
+
+  @media (max-width:800px){
+    border: 1px solid black;
+  }
+`;
 
 
 function Home() {
@@ -26,16 +67,17 @@ function Home() {
   return (
     <div className="home">
       <h1 style={styles.heading}>Tech Blog</h1>
+      {/* <BlogHeading>Tech Blog</BlogHeading> */}
       <div id ="blog-by">Sarthak</div>
 
       {posts.map((post,index) => (
-        <div className="post" key={`post-${index}`}>
+        <Post className="post" key={`post-${index}`}>
         <Link to={`/post/${post.id}`}>
           <h3> {post.title} </h3>
         </Link>
 
-        <p> {post.subTitle} </p>
-        </div>
+        <PostSubTitle> {post.subTitle} </PostSubTitle>
+        </Post>
       ))}
     </div>
   );
